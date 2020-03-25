b965f00c98da350ce6dc15bef6417e40f2c5fc97
Martin <martinpayne73@gmail.com>
2020-03-25 17:02:02 -0400

    add 48

---
+++ problem_0048_RotateImage.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public void Rotate(int[][] matrix)
+    {
+        var n = matrix.Length;
+
+        for (var outer = 0; outer < n / 2; outer++)
+        {
+            for (var x = outer; x < n - 1 - outer; x++)
+            {
+                var y = outer;
+
+                var temp = matrix[y][x];
+                matrix[y][x] = matrix[n - 1 - x][y];
+                matrix[n - 1 - x][y] = matrix[n - 1 - y][n - 1 - x];
+                matrix[n - 1 - y][n - 1 - x] = matrix[x][n - 1 - y];
+                matrix[x][n - 1 - y] = temp;
+            }
+        }
+    }
+}
