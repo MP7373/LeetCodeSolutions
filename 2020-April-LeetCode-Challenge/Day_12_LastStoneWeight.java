b095dbd5f36201a6d2e81a44adbf6b90675bc748
Martin <martinpayne73@gmail.com>
2020-04-12 17:23:11 -0400

    add day 12

---
+++ 2020-April-LeetCode-Challenge/Day_12_LastStoneWeight.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        var stonesQ = new PriorityQueue<Integer>(stones.length, (a, b) -> b - a);
+
+        for (var stone : stones) {
+            stonesQ.add(stone);
+        }
+        
+        while (stonesQ.size() > 1) {
+            var newStone = stonesQ.poll() - stonesQ.poll();
+            
+            if (newStone > 0) {
+                stonesQ.add(newStone);
+            }
+        }
+        
+        return stonesQ.size() > 0 ? stonesQ.peek() : 0;
+    }
+}
