aa6db1db624100b84f4fbbf2db97801e6b296af2
Martin <martinpayne73@gmail.com>
2020-04-14 11:21:34 -0400

    add day 14

---
+++ 2020-April-LeetCode-Challenge/Day_14_PerformStringShifts.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String stringShift(String s, int[][] shift) {
+        var finalShift = 0;
+        
+        for (var shif : shift) {
+            finalShift += shif[0] == 0 ? -shif[1] : shif[1];
+        }
+        
+        finalShift %= s.length();
+        
+        return finalShift >= 0 ?
+            s.substring(s.length() - finalShift) + s.substring(0, s.length() - finalShift) :
+            s.substring(-finalShift) + s.substring(0, -finalShift);
+    }
+}
