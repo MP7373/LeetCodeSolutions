13131f98ded0dc8b447d8984a736e31eb92b1256
Martin <martinpayne73@gmail.com>
2020-05-01 09:47:08 -0400

    add may day 1

---
+++ 2020-May-LeetCode-Challenge/FirstBadVersion.java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+  public int firstBadVersion(int n) {
+      return firstBadVersion(1, n);
+  }
+  
+  public int firstBadVersion(int left, int right) {
+      int mid = left + (right - left) / 2;
+      
+      var isBad = isBadVersion(mid);
+      
+      if (isBad && (mid == 1 || !isBadVersion(mid - 1))) {
+          return mid;
+      }
+      
+      if (isBad) {
+          return firstBadVersion(left, mid - 1);
+      }
+      
+      return firstBadVersion(mid + 1, right);
+  }
+}
