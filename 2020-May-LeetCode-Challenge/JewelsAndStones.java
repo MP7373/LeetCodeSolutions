38e604d588a4f47900c27756eccc2b5bd0bc665d
Martin <martinpayne73@gmail.com>
2020-05-02 08:45:49 -0400

    add may 2020 challenge day 2

---
+++ 2020-May-LeetCode-Challenge/JewelsAndStones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        var jewels = new HashSet<Character>();
+        
+        for (var c : J.toCharArray()) {
+            jewels.add(c);
+        }
+        
+        var numberOfJewels = 0;
+        for (var c : S.toCharArray()) {
+            if (jewels.contains(c)) {
+                numberOfJewels++;
+            }
+        }
+        
+        return numberOfJewels;
+    }
+}
