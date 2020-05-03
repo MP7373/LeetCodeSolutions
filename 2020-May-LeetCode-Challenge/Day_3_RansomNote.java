34654c34ab65e6a63a4fc161d20f0573b6eb337b
Martin <martinpayne73@gmail.com>
2020-05-03 09:03:34 -0400

    add day 3 ransom note

---
+++ 2020-May-LeetCode-Challenge/Day_3_RansomNote.java
@@ -0,0 +1,24 @@
+class Solution {
+  public boolean canConstruct(String ransomNote, String magazine) {
+      var noteCharMap = makeCharCountMapFromString(ransomNote);
+      var magazineCharMap = makeCharCountMapFromString(magazine);
+      
+      for (var c : noteCharMap.keySet()) {
+          if (magazineCharMap.getOrDefault(c, 0) < noteCharMap.get(c)) {
+              return false;
+          }
+      }
+      
+      return true;
+  }
+  
+  private HashMap<Character, Integer> makeCharCountMapFromString(String s) {
+      var map = new HashMap<Character, Integer>();
+
+      for (var c : s.toCharArray()) {
+          map.put(c, map.getOrDefault(c, 0) + 1);
+      }
+      
+      return map;
+  }
+}
