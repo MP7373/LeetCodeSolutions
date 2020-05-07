6584f2c65d96cae25ce1f7082995910af4b14c59
Martin <martinpayne73@gmail.com>
2020-05-07 10:43:17 -0400

    add day 7 may 2020 challenge

---
+++ 2020-May-LeetCode-Challenge/Day_7_CousinsInBinaryTree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        var xResult = findDepthAndParent(new ParentDepthChild(null, 0, root), x);
+        var yResult = findDepthAndParent(new ParentDepthChild(null, 0, root), y);
+
+        return xResult.depth == yResult.depth && xResult.parent != yResult.parent;
+    }
+    
+    private ParentDepthChild findDepthAndParent(ParentDepthChild parentDepthChild, int value) {
+        var node = parentDepthChild.child;
+
+        if (node == null) {
+            return null;
+        }
+        
+        if (node.val == value) {
+            return parentDepthChild;
+        }
+
+        var left = findDepthAndParent(new ParentDepthChild(node, parentDepthChild.depth + 1, node.left), value);
+        
+        return left != null ?
+            left :
+            findDepthAndParent(new ParentDepthChild(node, parentDepthChild.depth + 1, node.right), value);
+    }
+}
+
+class ParentDepthChild {
+    TreeNode parent;
+    int depth;
+    TreeNode child;
+    
+    ParentDepthChild(TreeNode p, int d, TreeNode c) {
+        parent = p;
+        depth = d;
+        child = c;
+    }
+}
