fc63265b915f3d5a2bca476afc26ee7eef852598
Martin <martinpayne73@gmail.com>
2020-05-17 21:11:51 -0400

    add may 2020 challenge day 17

---
+++ 2020-May-LeetCode-Challenge/Day_17_FindAllAnagramsInAString.java
@@ -0,0 +1,61 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        if (s.length() < p.length()) {
+            return new ArrayList<Integer>();
+        }
+
+        var pCharCounts = new HashMap<Character, Integer>();
+
+        for (var c : p.toCharArray()) {
+            pCharCounts.put(c, pCharCounts.getOrDefault(c, 0) + 1);
+        } 
+        
+        var matches = 0;
+        var neededMatches = pCharCounts.keySet().size();
+        var sSubstringCharCounts = new HashMap<Character, Integer>();
+        var anagramStartIndexes = new ArrayList<Integer>();
+
+        for (var i = 0; i < p.length(); i++) {
+            var c = s.charAt(i);
+
+            sSubstringCharCounts.put(c, sSubstringCharCounts.getOrDefault(c, 0) + 1);
+            
+            if ((int) sSubstringCharCounts.getOrDefault(c, 0) == (int) pCharCounts.getOrDefault(c, 0)) {
+                matches++;
+            } else if ((int) sSubstringCharCounts.getOrDefault(c, 0) - 1 == (int) pCharCounts.getOrDefault(c, 0)) {
+                matches--;
+            }
+            
+            if (matches == neededMatches) {
+                anagramStartIndexes.add(i - p.length() + 1);
+            }
+
+        }
+        
+        for (var i = p.length(); i < s.length(); i++) {
+            var firstChar = s.charAt(i - p.length());
+            sSubstringCharCounts.put(firstChar, Math.max(sSubstringCharCounts.get(firstChar) - 1, 0));
+            
+            if (sSubstringCharCounts.getOrDefault(firstChar, 0) == pCharCounts.getOrDefault(firstChar, 0)) {
+                matches++;
+            } else if (sSubstringCharCounts.getOrDefault(firstChar, 0) + 1 == pCharCounts.getOrDefault(firstChar, 0)) {
+                matches--;
+            }
+            
+            var c = s.charAt(i);
+            sSubstringCharCounts.put(c, sSubstringCharCounts.getOrDefault(c, 0) + 1);
+            
+            if ((int) sSubstringCharCounts.getOrDefault(c, 0) == (int) pCharCounts.getOrDefault(c, 0)) {
+                matches++;
+            } else if ((int) sSubstringCharCounts.getOrDefault(c, 0) - 1 == (int) pCharCounts.getOrDefault(c, 0)) {
+                matches--;
+            }
+            
+            if (matches == neededMatches) {
+                anagramStartIndexes.add(i - p.length() + 1);
+            }
+        }
+        
+        return anagramStartIndexes;
+    }
+}
