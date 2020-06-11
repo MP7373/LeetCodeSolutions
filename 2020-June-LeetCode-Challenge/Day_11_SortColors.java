060ad78c37e0a418c632695ff3b10e1b20a685a6
Martin <martinpayne73@gmail.com>
2020-06-11 11:41:21 -0400

    add day 11 June 2020 challenge

---
+++ 2020-June-LeetCode-Challenge/Day_11_SortColors.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for (int i = 0, zeroIndex = 0, twoIndex = nums.length - 1; i <= twoIndex; i++) {
+            if (nums[i] == 0) {
+                nums[i] = 1;
+                nums[zeroIndex++] = 0;
+            } else if (nums[i] == 2) {
+                nums[i--] = nums[twoIndex];
+                nums[twoIndex--] = 2;
+            }
+        }
+    }
+}
