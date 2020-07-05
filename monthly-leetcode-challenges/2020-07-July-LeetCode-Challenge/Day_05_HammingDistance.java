d356c88b73c03fc03037618fab4da879e4b1f0a4
Martin <martinpayne73@gmail.com>
2020-07-05 18:23:14 -0400

    add day 5 July 2020 challenge

---
+++ monthly-leetcode-challenges/2020-07-July-LeetCode-Challenge/Day_05_HammingDistance.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        var matchingBits = 0;
+
+        for (var i = 0; i < 31; i++) {
+            if ((1 & ~(x ^ y)) == 1) {
+                matchingBits++;
+            }
+
+            x >>= 1;
+            y >>= 1;
+        }
+        
+        return 31 - matchingBits;
+    }
+}
