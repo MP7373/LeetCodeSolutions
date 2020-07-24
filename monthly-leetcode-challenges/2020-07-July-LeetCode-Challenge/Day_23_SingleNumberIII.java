ad4f17dffc918a84cdd07ad2f10308c7e99809fc
Martin <martinpayne73@gmail.com>
2020-07-23 20:07:26 -0400

    add day 23 July 2020 challenge

---
+++ monthly-leetcode-challenges/2020-07-July-LeetCode-Challenge/Day_23_SingleNumberIII.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int mask = 0;
+        for (int num : nums) {
+            mask ^= num;
+        }
+
+        int firstValue = mask;
+        int mostRightOneMask = mask & -mask;
+        for (int num : nums) {
+            if ((mostRightOneMask & num) != 0) {
+                firstValue ^= num;
+            }
+        }
+
+        return new int[] { firstValue, firstValue ^ mask };
+    }
+}
